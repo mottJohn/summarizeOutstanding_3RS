9bce2422fd581983716b10ecaaa2fa124bb95647
Your Name <you@example.com>
2019-03-13 11:45:23 +0800

    added more columns and rearranged columns

--- MarineSummaryTable_userInputs.py
+++ MarineSummaryTable_userInputs.py
@@ -2,6 +2,15 @@ import pandas as pd
 import datetime
 import os
 
+#######
+#for fast checking
+#######
+
+xls = pd.ExcelFile('ET Inspection Register 3206.xlsx')
+xls_code = pd.ExcelFile('codesReference_3206.xlsx')
+contract_no = '3206'
+
+"""
 contract_no = input('Please enter contract number: ')
 xls_file = input('Please enter file path of the excel template: ')
 xls_file = os.path.abspath(xls_file)
@@ -11,7 +20,7 @@ xls_code = os.path.abspath(xls_code)
 
 xls = pd.ExcelFile(xls_file)
 xls_code = pd.ExcelFile(xls_code)
-
+"""
 code = xls_code.parse('Sheet1')
 
 #function to get the time delta between today and data of inspection
@@ -24,13 +33,18 @@ def filterOutstanding(barge_data, code):
     
     outstanding_barge_data = barge_data[(barge_data['Observations (O)/ Reminders (R)'] == 'O') & barge_data['Date of Closed Out'].isnull()] #filter
 
-    outstanding_barge_data_selected = outstanding_barge_data[['ID', 'Date of Inspection', 'Outstanding 1', 'Outstanding 2']] #select requiring columns
+    outstanding_barge_data_selected = outstanding_barge_data[['ID', 'Unnamed: 1', 'Unnamed: 2', 'Date of Inspection', 'Type of Inspection', 'Description', 'Outstanding 1', 'Outstanding 2', 'Remarks']] #select requiring columns
 
     outstanding_barge_data_selected['Outstanding Days'] = outstanding_barge_data_selected['Date of Inspection'].apply(lambda x: timedelta(x)) #calculate timedelta
 
     outstanding_barge_data_selected = pd.merge(code, outstanding_barge_data_selected, how='inner', left_on='Name of Barge (Short)', right_on='ID')
-    outstanding_barge_data_selected = outstanding_barge_data_selected.drop(columns = ['ID']) #drop duplicated column
+    outstanding_barge_data_selected['ID'] = outstanding_barge_data_selected['ID'].astype(str) + outstanding_barge_data_selected['Unnamed: 1'].astype(str) + outstanding_barge_data_selected['Unnamed: 2'].astype(int).astype(str) #concat to one column
+    outstanding_barge_data_selected = outstanding_barge_data_selected.drop(['Unnamed: 1', 'Unnamed: 2'], axis = 1) #drop columns
 
+    #change order of columns
+    cols = outstanding_barge_data_selected.columns.tolist()
+    cols = cols[:-2] +cols[-1:] + cols[-2:-1]
+    outstanding_barge_data_selected = outstanding_barge_data_selected[cols]
     return outstanding_barge_data_selected
 
 summaryTable = pd.DataFrame()
