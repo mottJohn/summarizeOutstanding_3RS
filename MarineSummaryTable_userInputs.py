e24a6e717b0683c243f3a28ab9b6af9b2aa5cafd
Your Name <you@example.com>
2019-03-13 12:02:45 +0800

    added MTRMPCAV

--- MarineSummaryTable_userInputs.py
+++ MarineSummaryTable_userInputs.py
@@ -62,9 +62,40 @@ for i in code['Name of Barge (Short)']:
 
 pivot = summaryTable.groupby(['Name of Barge','Name of Barge (Short)']).count()['Contract']
 pivot = pivot.rename(columns = {'Contract': 'Count of Outstanding Cases'})
-print("DONE!")
+
+#################################
+# FOR CAV
+#################################
+
+#function to filter outstanding cases
+def filterOutstanding_CAV(barge_data):
+    
+    outstanding_barge_data = barge_data[(barge_data['Observations (O)'] == 'O') & barge_data['Date of Closed Out'].isnull()] #filter
+
+    outstanding_barge_data_selected = outstanding_barge_data[['ID', 'Unnamed: 1', 'Unnamed: 2', 'Date', 'Type of deviation', 'Action done by Contractor']] #select requiring columns
+
+    outstanding_barge_data_selected['Date'] = pd.to_datetime(outstanding_barge_data_selected['Date'], format='%Y%m%d')
+    outstanding_barge_data_selected['Outstanding Days'] = outstanding_barge_data_selected['Date'].apply(lambda x: timedelta(x)) #calculate timedelta
+
+    outstanding_barge_data_selected['ID'] = outstanding_barge_data_selected['ID'].astype(str) + outstanding_barge_data_selected['Unnamed: 1'].astype(str) + outstanding_barge_data_selected['Unnamed: 2'].astype(int).astype(str) #concat to one column
+    outstanding_barge_data_selected = outstanding_barge_data_selected.drop(['Unnamed: 1', 'Unnamed: 2'], axis = 1) #drop columns
+
+    #change order of columns
+    #cols = outstanding_barge_data_selected.columns.tolist()
+    #cols = cols[:-2] +cols[-1:] + cols[-2:-1]
+    #outstanding_barge_data_selected = outstanding_barge_data_selected[cols]
+    return outstanding_barge_data_selected
+
+print("Working on {}".format('MTRMPCAV'))
+
+barge_data = xls.parse('MTRMPCAV' , header= 2, na_values = '', keep_default_na = False) #default na value is empty string, ignore default NaN
+barge_data['Observations (O)'] = barge_data['Observations (O)'].astype(str)
+filtered_data_CAV = filterOutstanding_CAV(barge_data)
 
 #save to excel with two sheets
 with pd.ExcelWriter('summaryData_{}.xlsx'.format(contract_no)) as writer:
     summaryTable.to_excel(writer, index = False, sheet_name='Summary Data')
     pivot.to_excel(writer, sheet_name='Summary Table')
+    filtered_data_CAV.to_excel(writer, index = False, sheet_name='Summary Data (MTRMPCAV)')
+
+print("DONE!")
